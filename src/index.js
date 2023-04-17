f8ababbd49950ce5a621eb11a4a265fcda3deaa8
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-17 13:50:23 -0700

    add url variable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import './css/styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+// const url = `https://opentdb.com/api.php?amount=10&category=27&difficulty=hard&type=multiple`
